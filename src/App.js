f9259f021d74ff3f0d42061c0068261a984a39d4
Mohamad Homsi <homsimohamad4@gmail.com>
2022-11-27 23:01:23 +0200

    lol

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { auth } from "./firebase";
+import { auth, db } from "./firebase";
 import Admin from "./Views/Admin/Admin";
 import Dashboard from "./Views/Admin/Dashboard/Dashboard";
 import Users from "./Views/Admin/Users/Users";
@@ -9,19 +9,30 @@ import Home from "./Views/Client/Home/Home";
 import NoMatch from "./Views/NoMatch/NoMatch";
 import Vendor from "./Views/Vendor/Vendor";
 import VendorDashboard from "./Views/Vendor/Dashboard/VendorDashboard";
+import Register from "./Views/Auth/Register/Register";
+import Login from "./Views/Auth/Login/Login";
 
-function App() {
+export default function App() {
   const [isUser, SetisUser] = React.useState(false);
+  const [user, SetUser] = React.useState({});
 
   React.useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
         SetisUser(true);
+        GetUser(user.uid);
       } else {
         SetisUser(false);
       }
     });
   }, []);
+
+
+  const GetUser = (uid) => {
+    db.collection("Users").doc(uid).onSnapshot((doc) => {
+      SetUser(doc.data());
+    });
+  };
 
   return (
     <div className="App">
@@ -33,7 +44,7 @@ function App() {
 
         {
           isUser &&
-            auth.currentUser.email === "admin@admin.com" ?
+            user.isAdmin ?
             <>
               <Route path="/admin">
                 <Route index element={<Admin children={<Dashboard />} />} />
@@ -44,16 +55,19 @@ function App() {
             :
             <></>
         }
-
+        {
+          !isUser &&
+          <Route>
+            <Route index path="/auth/register" element={<Register />} />
+            <Route index path="/auth/login" element={<Login />} />
+          </Route>
+        }
         <Route path="/vendor">
           <Route index element={<Vendor children={<VendorDashboard />} />} />
           <Route path="*" element={<NoMatch />} />
         </Route>
-
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
   );
 }
-
-export default App;
