871f3acdf7c0074fc85f2a6a1be77f18725502b9
Mohamad Homsi <homsimohamad4@gmail.com>
2022-11-28 01:53:31 +0200

    ..

--- src/Views/Admin/AdminNavBar/AdminNav.jsx
+++ src/Views/Admin/AdminNavBar/AdminNav.jsx
@@ -3,16 +3,42 @@ import "./Nav.css";
 import { HiOutlineHome, HiOutlineUsers } from "react-icons/hi";
 import { BiStoreAlt } from "react-icons/bi";
 import { AiOutlineStar } from "react-icons/ai";
+import { auth } from "../../../firebase";
 
 function AdminNav() {
   return (
     <div className="NavBar">
-      <div className="NavBarItem">
-        <h3>Menu</h3>
-        <a href="/admin"><HiOutlineHome/> Home</a>
-        <a href="/admin/users"><HiOutlineUsers/> Users</a>
-        <a href="/admin/vendors"><BiStoreAlt/> Vendors</a>
-        <a href="/admin/reviews"><AiOutlineStar/> Reviews</a>
+      <div>
+        <h3 className="mainNavTitle">Munasabti Admin</h3>
+        <div className="NavBarItem">
+          <h3 className="InnerTitle">Menu</h3>
+          <a href="/admin">
+            <HiOutlineHome /> Dashboard
+          </a>
+          <a href="/admin/users">
+            <HiOutlineUsers /> Users
+          </a>
+          <a href="/admin/vendors">
+            <BiStoreAlt /> Vendors
+          </a>
+          <a href="/admin/vendors">
+            <BiStoreAlt /> Events
+          </a>
+          <a href="/admin/reviews">
+            <AiOutlineStar /> Website Reviews
+          </a>
+        </div>
+      </div>
+      <div className="bottomLogout">
+        <p
+          onClick={() => {
+            auth.signOut().then(() => {
+              document.location.href = "/";
+            });
+          }}
+        >
+          Logout
+        </p>
       </div>
     </div>
   );
--- src/Views/Admin/Dashboard/Dashboard.jsx
+++ src/Views/Admin/Dashboard/Dashboard.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "./Dashboard.css";
 
 function Dashboard() {
-  return <div>Lol</div>;
+  return (
+    <div className="AdminDash">
+      <div className="topBar">
+        <h1 className="titleDash">Dashboard</h1>
+      </div>
+      <div className="firstRow">
+        <div className="firstRowItem">
+          <h2>Users</h2>
+          <p>Number of users</p>
+        </div>
+        <div className="firstRowItem">
+          <h2>Vendors</h2>
+          <p>Number of vendors</p>
+        </div>
+        <div className="firstRowItem">
+          <h2>Events</h2>
+          <p>Number of reviews</p>
+        </div>
+        <div className="firstRowItem">
+          <h2>Events</h2>
+          <p>Number of reviews</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
