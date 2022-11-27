2a4d4f057a7b10e26b0f3e793ade3f96c861dbe0
SaraBaassiri <sarabsr95@gmail.com>
2022-11-28 01:50:57 +0300

    more navbar styling

--- src/Views/Admin/AdminNavBar/AdminNav.jsx
+++ src/Views/Admin/AdminNavBar/AdminNav.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import "./Nav.css";
 import { HiOutlineHome, HiOutlineUsers } from "react-icons/hi";
 import { BiStoreAlt } from "react-icons/bi";
+import { AiOutlineStar } from "react-icons/ai";
 
 function AdminNav() {
   return (
     <div className="NavBar">
       <div className="NavBarItem">
+        <h3>Menu</h3>
         <a href="/admin"><HiOutlineHome/> Home</a>
         <a href="/admin/users"><HiOutlineUsers/> Users</a>
-        <a href="/admin/clients"><BiStoreAlt/> Vendors</a>
+        <a href="/admin/vendors"><BiStoreAlt/> Vendors</a>
+        <a href="/admin/reviews"><AiOutlineStar/> Reviews</a>
       </div>
     </div>
   );
