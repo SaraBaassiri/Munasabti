9f3490ce75b06606ef368bf09a7c29ffb3ed2c46
SaraBaassiri <sarabsr95@gmail.com>
2022-11-28 01:26:45 +0300

    navbar styling

--- src/Views/Admin/AdminNavBar/AdminNav.jsx
+++ src/Views/Admin/AdminNavBar/AdminNav.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Nav.css";
+import { HiOutlineHome } from "react-icons/hi";
 
 function AdminNav() {
   return (
     <div className="NavBar">
       <div className="NavBarItem">
-        <a href="/admin">Home</a>
+        <a href="/admin"><HiOutlineHome/> Home</a>
         <a href="/admin/users">Users</a>
-        <a href="/admin/clients">Clients</a>
+        <a href="/admin/clients">Vendors</a>
       </div>
     </div>
   );
