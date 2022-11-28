aba9dc95ce5993e3bfe0710fe30ba8389a0d7366
Mohamad Homsi <homsimohamad4@gmail.com>
2022-11-28 10:11:07 +0200

    Fixed Backend

--- src/Views/Admin/Dashboard/Dashboard.jsx
+++ src/Views/Admin/Dashboard/Dashboard.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 import "./Dashboard.css";
+import { auth, db } from "../../../firebase";
 
 function Dashboard() {
+  const [users, setUsers] = React.useState(0);
+  const [vendors, setVendors] = React.useState(0);
+  const [reviews, setReviews] = React.useState(0);
+  const [events, setEvents] = React.useState(0);
+
+  React.useEffect(() => {
+    db.collection("Users")
+      .get()
+      .then((doc) => {
+        setUsers(doc.size);
+      });
+
+    db.collection("Vendors")
+      .get()
+      .then((doc) => {
+        setVendors(doc.size);
+      });
+
+    db.collection("Reviews")
+      .get()
+      .then((doc) => {
+        setReviews(doc.size);
+      });
+
+    db.collection("Events")
+      .get()
+      .then((doc) => {
+        setEvents(doc.size);
+      });
+  }, []);
+
   return (
     <div className="AdminDash">
       <div className="topBar">
@@ -10,19 +42,19 @@ function Dashboard() {
       <div className="firstRow">
         <div className="firstRowItem">
           <h2>Users</h2>
-          <p>Number of users</p>
+          <p>{users}</p>
         </div>
         <div className="firstRowItem">
           <h2>Vendors</h2>
-          <p>Number of vendors</p>
+          <p>{vendors}</p>
+        </div>
+        <div className="firstRowItem">
+          <h2>Reviews</h2>
+          <p>{reviews}</p>
         </div>
         <div className="firstRowItem">
           <h2>Events</h2>
-          <p>Number of reviews</p>
-        </div>
-        <div className="firstRowItem">
-          <h2>Events</h2>
-          <p>Number of reviews</p>
+          <p>{events}</p>
         </div>
       </div>
     </div>
