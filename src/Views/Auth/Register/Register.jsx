aab6a306ad6f625b6f1e9c4056d01ce3e197d6b3
SaraBaassiri <sarabsr95@gmail.com>
2022-12-02 22:51:50 +0300

    register page temp styling

--- src/Views/Auth/Register/Register.jsx
+++ src/Views/Auth/Register/Register.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./Register.css";
 
 function Register() {
-  return <div>Register</div>;
+  return <div>
+    <div className="regContainer">
+      <h2 className="header">Temp Register</h2>
+      <input type="email" placeholder="Enter email..." id="email" /> <br />
+      <input type="text" placeholder="Enter first name..." id="Fname" /> <br />
+      <input type="text" placeholder="Enter last name..." id="Lname" /> <br />
+      <input type="password" placeholder="Enter password" id="pass" /> <br />
+      <input type="password" placeholder="Confirm password" id="conPass" /> <br />
+      <button id="btn">Register</button>
+    </div>
+  </div>;
 }
 
 export default Register;
