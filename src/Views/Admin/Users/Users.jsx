6b7fa813f5a97cdccef9d6319ee8d05b3d249c09
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-02 21:41:11 +0200

    ..

--- src/Views/Admin/Users/Users.jsx
+++ src/Views/Admin/Users/Users.jsx
@@ -1,20 +1,66 @@
 import React from "react";
+import { auth, db } from "../../../firebase";
 
 function Users() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setItems([]);
+    await db
+      .collection("Users")
+      .get()
+      .then((snapshot) => {
+        if (!snapshot.empty) {
+          let item = [];
+          snapshot.forEach((doc) => {
+            let data = Object.assign({ id: doc.id }, doc.data());
+            item.push(data);
+          });
+          setItems(item);
+        }
+      });
+  };
+
   return (
     <div className="AdminUsers">
       <div className="topBar">
         <h1 className="titleUsers">Users</h1>
       </div>
       <table className="UsersTable">
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email</th>
-          <th>Phone Number</th>
-          <th>Actions</th>
-        </tr>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+          </tr>
+          <tr>
+            <td>Anom</td>
+            <td>19</td>
+            <td>Male</td>
+          </tr>
+          <tr>
+            <td>Megha</td>
+            <td>19</td>
+            <td>Female</td>
+          </tr>
+          <tr>
+            <td>Subham</td>
+            <td>25</td>
+            <td>Male</td>
+          </tr>
+        </tbody>
       </table>
+      {items.map((item) => {
+        return (
+          <div>
+            <h1>{item.email}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
