8c3c243ee1a2065f44554fd4eba238d0244a6d02
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-04 22:28:18 +0200

    Update Register.jsx

--- src/Views/Auth/Register/Register.jsx
+++ src/Views/Auth/Register/Register.jsx
@@ -1,18 +1,103 @@
 import React from "react";
 import "./Register.css";
+import { auth, db } from "../../../firebase";
+import { setTrue, setFalse } from "../../../Redux/reducers/loadingSlice";
+import { setData } from "../../../Redux/reducers/userSlice";
+import { useDispatch } from "react-redux";
 
 function Register() {
-  return <div>
-    <div className="regContainer">
-      <h2 className="header">Temp Register</h2>
-      <input type="email" placeholder="Enter email..." id="email" /> <br />
-      <input type="text" placeholder="Enter first name..." id="Fname" /> <br />
-      <input type="text" placeholder="Enter last name..." id="Lname" /> <br />
-      <input type="password" placeholder="Enter password" id="pass" /> <br />
-      <input type="password" placeholder="Confirm password" id="conPass" /> <br />
-      <button id="btn">Register</button>
+  const dispatch = useDispatch();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [name, setName] = React.useState("");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    dispatch(setTrue());
+    if (password === confirmPassword) {
+      const data = Object.assign({
+        email: email,
+        name: name,
+        isAdmin: false,
+        isVendor: false,
+      });
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then((auth) => {
+          db.collection("Users")
+            .doc(auth.user.uid)
+            .set({
+              email: email,
+              name: name,
+              isAdmin: false,
+              isVendor: false,
+            })
+            .then(() => {
+              dispatch(setData(data));
+              dispatch(setFalse());
+              document.location.href = "/";
+            })
+            .catch((e) => {
+              dispatch(setFalse());
+              alert(e.message);
+            });
+        })
+        .catch((e) => {
+          dispatch(setFalse());
+          alert(e.message);
+        });
+    } else {
+      dispatch(setFalse());
+      alert("Passwords do not match");
+    }
+  };
+
+  return (
+    <div>
+      <div className="regContainer">
+        <h2 className="header">Temp Register</h2>
+        <form onSubmit={handleRegister}>
+          <input
+            type="email"
+            placeholder="Enter email..."
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Enter first name..."
+            id="Fname"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Enter last name..."
+            id="Lname"
+            onChange={(e) => setName(`${name} ${e.target.value}`)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Enter password"
+            id="pass"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Confirm password"
+            id="conPass"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <br />
+          <button id="btn">Register</button>
+        </form>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Register;
