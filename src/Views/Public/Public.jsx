c29450468c2eed00ed4be4f1963c00941f220ead
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-21 22:39:51 +0200

    ..

--- src/Views/Public/Public.jsx
+++ src/Views/Public/Public.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./Footer/Footer";
 import PublicNav from "./PublicNav/PublicNav";
 
 function Public({ children }) {
@@ -12,6 +13,7 @@ function Public({ children }) {
       >
         {children}
       </div>
+      <Footer />
     </div>
   );
 }
