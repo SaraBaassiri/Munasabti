db49fc8d391d89606bc6e836c7016873ef2b1d12
SaraBaassiri <sarabsr95@gmail.com>
2022-12-22 00:29:01 +0300

    footer

--- src/Views/Public/Footer/Footer.jsx
+++ src/Views/Public/Footer/Footer.jsx
@@ -1,6 +1,29 @@
 import React from "react";
 import "./Footer.css";
+import Logo from "../../../images/logov2.svg"
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="Footer">
+      <div className="line"/>
+      <div className="Logo">
+        <img src={Logo} alt="logo" />
+      </div>
+      <div className="Links">
+        <div>
+          <a href="/">Home</a>
+          <a href="/">About Us</a>
+          <a href="/">Services</a>
+          <a href="/">Vendors</a>
+          <a href="/">Inspirations</a>
+          <a href="/">Contact Us</a>
+        </div>
+      </div>
+      <div className="partTwo">
+        <a href="/">loremipsum@domain.com</a>
+        <a href="/">00 - 123456</a>
+      </div>
+      <h6>Copyright 2022</h6>
+    </div>
+  );
 }
