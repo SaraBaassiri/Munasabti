b2e7bbacbec3e4fd33cf484a11c3ad0589fb37f8
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-28 22:31:42 +0200

    Update Vendors.jsx

--- src/Views/Public/Vendors/Vendors.jsx
+++ src/Views/Public/Vendors/Vendors.jsx
@@ -15,6 +15,10 @@ export default function Vendors() {
   const navigate = useNavigate();
   const swiperRef = React.useRef(null);
 
+  React.useEffect(() => {
+    document.title = "Munasabti | Vendors";
+  }, []);
+
   const tempData = [
     {
       id: 1,
