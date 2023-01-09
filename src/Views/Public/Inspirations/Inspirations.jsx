329a60d2e7f81aecc98754f2c5c988d5e5446035
SaraBaassiri <sarabsr95@gmail.com>
2023-01-10 00:07:32 +0300

    inspirations page

---
+++ src/Views/Public/Inspirations/Inspirations.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Inspirations(props) {
+  return (
+    <div>
+      <h1>Inspirations</h1>
+      <div>{props.children}</div>
+    </div>
+  );
+}
+
+export default Inspirations;
